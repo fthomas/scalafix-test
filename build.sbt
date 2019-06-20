a1596b02420dc484ecba4600a9d36d87942c4d21
Scala <me@scala-steward.org>
2019-06-20 20:51:38 +0200

    Update http4s-core, http4s-dsl to 0.20.3

--- build.sbt
+++ build.sbt
@@ -2,6 +2,6 @@ name := "scalafix-test"
 
 libraryDependencies := List(
   "co.fs2" %% "fs2-core" % "0.10.7",
-  "org.http4s" %% "http4s-core" % "0.18.24",
-  "org.http4s" %% "http4s-dsl" % "0.18.24"
+  "org.http4s" %% "http4s-core" % "0.20.3",
+  "org.http4s" %% "http4s-dsl" % "0.20.3"
 )
--- src/main/scala/Http4sService.scala
+++ src/main/scala/Http4sService.scala
@@ -1,9 +1,9 @@
 import cats.effect.IO
-import org.http4s.HttpService
+import org.http4s.HttpRoutes
 import org.http4s.dsl.io._
 
 object Http4sService {
-  def service: HttpService[IO] = HttpService[IO] {
-    case _ @ GET -> Root => Ok().withBody("hello")
+  def service: HttpRoutes[IO] = HttpRoutes.of[IO] {
+    case _ @ GET -> Root => Ok().map(_.withEntity("hello"))
   }
 }
