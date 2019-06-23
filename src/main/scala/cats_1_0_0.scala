72b30969a6e031bc2ff0b7da8cbd17f60e45d11a
Frank S. Thomas <frank@timepit.eu>
2019-06-23 17:08:17 +0200

    Add some scio code

--- src/main/scala/cats_1_0_0.scala
+++ src/main/scala/cats_1_0_0.scala
@@ -1,6 +1,6 @@
 import cats.implicits._
 
-object Cats {
+object cats_1_0_0 {
 
   (Option(1) |@| Option(2) |@| Option(3)).map(_ + _ + _)
 
---
+++ src/main/scala/scio_0_7_0.scala
@@ -0,0 +1,30 @@
+import com.spotify.scio.values.SCollection
+import org.apache.avro.generic.GenericRecord
+import scala.reflect.ClassTag
+
+// https://github.com/spotify/scio/blob/master/site/src/paradox/migrations/v0.7.0-Migration-Guide.md#automated-migration
+object scio_0_7_0 {
+
+  object MyScioJob {}
+
+  val inputAUri = ""
+  val inputBUri = ""
+  val output = "output"
+
+  object Schemas {
+    def record1: GenericRecord = ???
+
+    def record2: GenericRecord = ???
+  }
+
+  implicit def bo: com.spotify.scio.testing.JobTest.BeamOptions =
+    ??? // XXX: why do I need this ?
+
+  def doSomething[T: ClassTag](coll: SCollection[T]): SCollection[T] =
+    coll.map { t =>
+      // do something that returns a T
+      val result: T = ???
+      result
+    }
+
+}
